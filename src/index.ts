b385f92daa4db1d0f03c60b3c88f2dc9e9874e3c
Amit Rana <amit5885rana@gmail.com>
2026-02-11 18:08:29 +0545

    init

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import { prisma } from "./client";
+
+const app = express();
+app.use(express.json());
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
+});
+
+app.post("/users", async (req, res) => {
+  const { username, password } = req.body;
+  const user = await prisma.user.create({
+    data: {
+      username,
+      password,
+    },
+  });
+  res.status(200).json({ message: "User created successfully", id: user.id });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
